f78bdde17a53dc09cdf9c150799e3256ffa9074c
Eli Fatsi <eli.fatsi@viget.com>
2016-09-16 11:08:14 -0600

    Only show existing answers on admin view

--- web/static/js/components/admin/answers.js
+++ web/static/js/components/admin/answers.js
@@ -17,31 +17,17 @@ let Answers = React.createClass({
     let { round_info, channel } = this.props
     let answers = round_info.answers
 
-    return [0,1,2,3,4,5].map(function(index) {
-      let answer = answers[index]
-
-      if (answer) {
-        return (
-          <li className={ this.class_name(answer) } key={ index }>
-            <Answer channel={ channel } answer={ answer } index={ index } done={ round_info.done } />
-          </li>
-        )
-      } else {
-        return (
-          <li className={ this.class_name(answer) } key={ index }>
-            { index + 1 }
-          </li>
-        )
-      }
+    return answers.map(function(answer, index) {
+      return (
+        <li className={ this.class_name(answer) } key={ index }>
+          <Answer channel={ channel } answer={ answer } index={ index } done={ round_info.done } />
+        </li>
+      )
     }.bind(this))
   },
 
   class_name(answer) {
-    if (answer === undefined) {
-      return "answer no-answer"
-    } else {
-      return "answer answer-" + (answer.used ? "used" : "unused")
-    }
+    return "answer answer-" + (answer.used ? "used" : "unused")
   },
 })
 
