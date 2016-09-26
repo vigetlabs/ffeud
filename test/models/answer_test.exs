b0c9fef7aa85cedb3487527827fb2d05da3d85a1
Eli Fatsi <eli.fatsi@viget.com>
2016-09-26 14:48:32 -0600

    Updated answer_test to use setup

--- test/models/answer_test.exs
+++ test/models/answer_test.exs
@@ -1,4 +1,3 @@
-import IEx
 defmodule FamilyFeud.AnswerTest do
   use FamilyFeud.ModelCase
 
@@ -15,43 +14,39 @@ defmodule FamilyFeud.AnswerTest do
     refute changeset.valid?
   end
 
-  test "creating the 8th answer" do
+  setup do
     {:ok, user}  = User.create(User.changeset(%User{}, %{"email" => "e@example.com", "password" => "password"}))
     {:ok, game}  = Game.create(%{"name" => "Test Game"}, user)
     {:ok, round} = Round.create(%{"name" => "Test Round", "question" => "What?", "position" => 1}, game)
 
-    Enum.each 1..7, fn(i) ->
-      {:ok, answer} = Answer.create(%{"body" => "This", "points" => 1}, round)
+    {:ok, round: round}
+  end
+
+  test "creating the 8th answer", context do
+    Enum.each 1..7, fn(_i) ->
+      {:ok, _answer} = Answer.create(%{"body" => "This", "points" => 1}, context[:round])
     end
 
-    changeset = Answer.changeset(%Answer{}, Map.put(@valid_attrs, :round_id, round.id))
+    changeset = Answer.changeset(%Answer{}, Map.put(@valid_attrs, :round_id, context[:round].id))
     assert changeset.valid?
   end
 
-  test "creating the 9th answer" do
-    {:ok, user}  = User.create(User.changeset(%User{}, %{"email" => "e@example.com", "password" => "password"}))
-    {:ok, game}  = Game.create(%{"name" => "Test Game"}, user)
-    {:ok, round} = Round.create(%{"name" => "Test Round", "question" => "What?", "position" => 1}, game)
-
-    Enum.each 1..8, fn(i) ->
-      {:ok, answer} = Answer.create(%{"body" => "This", "points" => 1}, round)
+  test "creating the 9th answer", context do
+    Enum.each 1..8, fn(_i) ->
+      {:ok, _answer} = Answer.create(%{"body" => "This", "points" => 1}, context[:round])
     end
 
-    changeset = Answer.changeset(%Answer{}, Map.put(@valid_attrs, :round_id, round.id))
+    changeset = Answer.changeset(%Answer{}, Map.put(@valid_attrs, :round_id, context[:round].id))
     refute changeset.valid?
   end
 
-  test "editing the 8th answer" do
-    {:ok, user}  = User.create(User.changeset(%User{}, %{"email" => "e@example.com", "password" => "password"}))
-    {:ok, game}  = Game.create(%{"name" => "Test Game"}, user)
-    {:ok, round} = Round.create(%{"name" => "Test Round", "question" => "What?", "position" => 1}, game)
-
-    answers = Enum.map 1..8, fn(i) ->
-      {:ok, answer} = Answer.create(%{"body" => "This", "points" => 1}, round)
+  test "editing the 8th answer", context do
+    answers = Enum.map 1..8, fn(_i) ->
+      {:ok, answer} = Answer.create(%{"body" => "This", "points" => 1}, context[:round])
       answer
     end
 
-    changeset = Answer.changeset(List.last(answers), Map.put(@valid_attrs, :round_id, round.id))
+    changeset = Answer.changeset(List.last(answers), Map.put(@valid_attrs, :round_id, context[:round].id))
     assert changeset.valid?
   end
 end
